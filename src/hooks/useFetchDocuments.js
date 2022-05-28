336a3217abcfdacfafd526d6daa690dc17e9b5dc
RaimundoJSoares <raianimes1000@gmail.com>
2022-05-27 20:54:04 -0400

    criando dashboard

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -32,6 +32,9 @@ export const useFetchDocuments = (docCollection, search=null, uid=null) => {
                 if(search) {  
                     q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"))
 
+                }else if(uid) {
+                    q = await query(collectionRef, where("uid" , "==" , uid), orderBy("createdAt", "desc"))
+
                 }else {
                     q = await query(collectionRef, orderBy('createdAt', 'desc'))
                 }
@@ -58,7 +61,7 @@ export const useFetchDocuments = (docCollection, search=null, uid=null) => {
         }
         loadData()
 
-    }, [docCollection,document, search, uid, cancelled])
+    }, [docCollection, search, uid, cancelled])
 
     useEffect(() => {
         return() => setCanceled(true)
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,52 @@
-import React from 'react'
-import styles from './Dashboard.module.css'
+import { Link } from "react-router-dom";
+import styles from "./Dashboard.module.css";
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1> oi </h1>
-    </div>
-  )
-}
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-export default Dashboard
+  //user's post
+  const { document: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {
+  }
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Manage your posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.nopost}>
+          <p>Posts don't found </p>
+          <Link className="btn" to="/posts/create">
+            Create post
+          </Link>
+        </div>
+      ) : (
+        <>
+          <div className={styles.post_header}>
+            <span>Title</span>
+            <span>Actions</span>
+          </div>
+          {posts && posts.map((post) => <div className={styles.post_row} key={post.id}>
+            <p>{post.title}</p>
+
+            <div>
+              <Link to={`/posts/${post.id}`} className='btn btn-outline'>Visualize</Link>
+              <Link to={`/posts/edit/${post.id}`} className='btn btn-outline'>Edit</Link>
+              <button onClick={() => deleteDocument(post.id)} className='btn btn-outline btn-danger'>Delete</button>
+            </div>
+          </div>)}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
